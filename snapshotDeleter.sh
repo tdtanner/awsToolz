1a627d23220cad18f63d6f31399de81df2bad36f
tdtanner <53793738+tdtanner@users.noreply.github.com>
2020-07-27 12:39:15 -0700

    Update snapshotDeleter.sh

--- snapshotDeleter.sh
+++ snapshotDeleter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-snapshots_to_delete=$(aws --profile oldprod ec2 describe-snapshots --owner-ids 357618474061 --query 'Snapshots[?StartTime<=`2020-06-27`].SnapshotId' --output json | tr -d '"' | tr ',' '\n' | tr -d '[]' | sed '/^$/d')
+snapshots_to_delete=$(aws --profile oldprod ec2 describe-snapshots --owner-ids xxxxxxxxxxx --query 'Snapshots[?StartTime<=`2020-06-27`].SnapshotId' --output json | tr -d '"' | tr ',' '\n' | tr -d '[]' | sed '/^$/d')
 # adjust your date range by modifying the string after StartTime
 echo "List of snapshots to delete: $snapshots_to_delete"
 # delete dem shits dont forget to add the right profile or I will keeeeel you
